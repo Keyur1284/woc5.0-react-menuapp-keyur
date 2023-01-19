265ddfb4e61733898d4f329e7098766e506de3a0
Govrani Keyur <202101498@daiict.ac.in>
2023-01-19 23:48:08 +0530

    Made Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/navbar';
+import { Home } from './pages/home';
+import { Food } from './pages/food';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/food">
+            <Route default path="" element={<Food type="all" />} />
+            <Route path="breakfast" element={<Food type="breakfast" />} />
+            <Route path="lunch" element={<Food type="lunch" />} />
+            <Route path="dinner" element={<Food type="dinner" />} />
+            <Route path="veg" element={<Food type="veg" />} />
+            <Route path="nonveg" element={<Food type="nonveg" />} />
+          </Route>
+          <Route path="*" element={<p className="display-1 text-danger">Error 404! Page not Found!</p>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
