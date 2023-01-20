939060d831ff534276c3a6acddd535eae4ed8292
Govrani Keyur <202101498@daiict.ac.in>
2023-01-20 23:45:49 +0530

    Modified the page

--- src/pages/food.js
+++ src/pages/food.js
@@ -38,19 +38,19 @@ export const Food = (params) => {
     }, [params]);
 
     return (
-        <div className="d-flex justify-content-start row row-cols-1 row-cols-md-3 g-3 App keyur">
+        <div className="justify-content-start row row-cols-1 bg-dark row-cols-md-3 g-3">
             {
                 foods?.map((food, index) => (
                     <div className="col "  key={index}>
-                    <div className="card h-100 text-start  mt-3">
-                        <img src={food.image} className="card-img-top " alt={food.name} />
+                    <div className="card h-100 text-start bg-dark text-white m-2 p-5">
+                        <img src={food.image} className="card-img-top h-50 w-100" alt={food.name} />
                         <div className="card-body"> 
                             <h5 className="card-title display-6">Item Name: {food.name}</h5>
                             <h6 className="card-text">Price: {food.price}</h6>
                             <h6 className="card-text">Description: {food.description}</h6>
                             <h6 className="card-text">Food type: {food.eatable? "Vegetarian" : "Non-vegetarian"}, {food.type}</h6>
                             <h6 className="card-text">Rating: {food.rating}/10</h6>
-                            <a className="btn btn-primary" href={food.video} target="_blank" >Recipe video</a>
+                            <a className="btn btn-info" href={food.video} target="_blank" >Recipe video</a>
                         </div>
                     </div>
                     </div>
