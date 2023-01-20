63e702de06992a21098aa7864fce853ed4ec36a6
Govrani Keyur <202101498@daiict.ac.in>
2023-01-20 23:45:07 +0530

    updated homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,8 @@ export const Home = () => {
     return (
         <div className="container keyur justify-content-center">
             <div className="row">
-                <div className="col">
-                    <h1 className="display-1">Welcome to KIWI Foods!</h1>
-                    <p className="lead">We are here to help you find the best food recipes for your daily needs.</p>
-                    <p className="lead">We have a wide variety of recipes for you to choose from.</p>
-                    <p className="lead">We have recipes for breakfast, lunch and dinner.</p>
-                    <p className="lead">We have recipes for both vegetarians and non-vegetarians.</p>
+                <div className="col display-1">
+                    Home Page
                 </div>
             </div>
         </div>
