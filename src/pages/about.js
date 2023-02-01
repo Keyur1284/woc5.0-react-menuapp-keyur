f3c309e85ae40035f41386f119a45a0c4f608a15
Govrani Keyur <202101498@daiict.ac.in>
2023-02-01 08:38:58 +0530

    made an about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
 export const About = () => {
     return (
-    <div className="container keyur justify-content-center">
-            <div className="row">
-                <div className="col">
-                    <h1 className="display-1">Welcome to KIWI Foods!</h1>
+    <div className="keyur justify-content-center">
+            <div className="row p-2">
+                <div className="col rounded-3" style={{marginTop: "170px", color:"#11303F", backgroundColor:"white", opacity: "70%", height: "300px", width: "900px"}}>
+                    <h1 className="display-1 mt-2">Welcome to KIWI Foods!</h1>
                     <p className="lead">We are here to help you find the best food recipes for your daily needs.</p>
                     <p className="lead">We have a wide variety of recipes for you to choose from.</p>
                     <p className="lead">We have recipes for breakfast, lunch and dinner.</p>
