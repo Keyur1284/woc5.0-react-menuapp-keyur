be947bc7f4fbdc847be779e2423495cc809c9cfc
Govrani Keyur <202101498@daiict.ac.in>
2023-02-02 09:43:38 +0530

    made a sample review page

---
+++ src/pages/reviews.js
@@ -0,0 +1,8 @@
+export const Reviews = () => {
+    return (
+        <div>
+            <h1 className="display-1 text-center text-light">Reviews</h1>
+            <p className="display-1 text-center text-light">Please login to read the reviews</p>
+        </div>
+    )
+}
