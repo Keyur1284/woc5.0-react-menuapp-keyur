0ff0400e53ab77a9d268df64505bbd4297280a3d
Govrani Keyur <202101498@daiict.ac.in>
2023-02-02 22:58:13 +0530

    add data to git ignore

--- src/data.js
+++ src/data.js
@@ -6,14 +6,14 @@ const foodRef = collection(db, "food");
 const addData = async () => {
 
     let data = {
-        name: "Sausage Strata",
-        price: 90,
-        description: "Tasty egg casserole that husband approves! I frequently make this dish because it's so easy to make. This recipe can be easily doubled or tripled, but you may have to cook it longer.",
-        eatable: false,
-        type: "Breakfast",
-        rating: 8.1,
-        video: "https://www.youtube.com/watch?v=g0A_WpD7wWI",
-        image: "https://www.allrecipes.com/thmb/LnhLm0aOuo9YUcMNLDz72zqnBsY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/4516609-87a9bdd46fb14bb1ad853b2185e81d92.jpg"
+        name: "Noodles",
+        price: "₹150",
+        description: "Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings. Noodles are usually cooked in boiling water, sometimes with cooking oil or salt added.",
+        eatable: true,
+        type: "Dinner",
+        rating: 7.9,
+        video: "https://www.youtube.com/watch?v=ulhRORJpuBM",
+        image: "https://static.toiimg.com/photo/79693966.cms"
     }
         console.log(data);
     data = await addDoc(foodRef, {
