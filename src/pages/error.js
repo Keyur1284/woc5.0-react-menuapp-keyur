e7282a93ab058accc9d90adb5427ee6801a15d40
Govrani Keyur <202101498@daiict.ac.in>
2023-02-02 22:26:51 +0530

    make error page

---
+++ src/pages/error.js
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
+export const Error = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="container" style={{height: "42.9vh"}}>
+            <p className="display-1 text-danger">Error 404! Page not Found!</p>
+                    <button className="btn btn-success btn-lg btn-block mt-2" type="submit" onClick={() => navigate("/")}>Go Back to Home</button>        
+        </div>
+    )
+}
