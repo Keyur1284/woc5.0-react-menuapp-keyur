059e023ff143d7a3213eb64bf19ab49c562262cf
Govrani Keyur <202101498@daiict.ac.in>
2023-02-03 16:13:51 +0530

    Updates

---
+++ precache-manifest.69b9e67f84ea828becd4915e5cdf39d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3604c91bbe2dd232f5d",
+    "url": "/woc5.0-react-menuapp-keyur/static/css/main.4f3d61ec.chunk.css"
+  },
+  {
+    "revision": "d3604c91bbe2dd232f5d",
+    "url": "/woc5.0-react-menuapp-keyur/static/js/main.d3604c91.chunk.js"
+  },
+  {
+    "revision": "93c748a80b187c4a0720",
+    "url": "/woc5.0-react-menuapp-keyur/static/js/1.93c748a8.chunk.js"
+  },
+  {
+    "revision": "80cf130b1bef7ddb49e3",
+    "url": "/woc5.0-react-menuapp-keyur/static/js/2.80cf130b.chunk.js"
+  },
+  {
+    "revision": "cfe8aa73b40635a5e584",
+    "url": "/woc5.0-react-menuapp-keyur/static/js/runtime~main.cfe8aa73.js"
+  },
+  {
+    "revision": "1b038978cfb51a0270d9d03deda2178e",
+    "url": "/woc5.0-react-menuapp-keyur/index.html"
+  }
+];
